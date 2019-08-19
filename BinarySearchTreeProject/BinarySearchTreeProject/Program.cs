ba104502b487cad3d31877d5b13af2b276948853
Nathaniel Kinderman <natekinderman16@gmail.com>
2019-08-19 14:56:48 -0500

    created node class and member variable

---
+++ BinarySearchTreeProject/BinarySearchTreeProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTreeProject
+{
+    class Node
+    {
+        int data;
+        Node leftChild;
+        Node rightChild;
+        public Node(int data)
+        {
+            this.data = data;
+            this.leftChild = null;
+            this.rightChild = null;
+        }
+        
+        
+    }
+}
