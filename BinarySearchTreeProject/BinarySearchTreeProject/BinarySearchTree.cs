91a6c696c1f64bbba623907fd165484f820ef486
Nathaniel Kinderman <natekinderman16@gmail.com>
2019-08-19 20:35:38 -0500

    BTS method attempted. Get it working and make it better

--- BinarySearchTreeProject/BinarySearchTreeProject/BinarySearchTree.cs
+++ BinarySearchTreeProject/BinarySearchTreeProject/BinarySearchTree.cs
@@ -8,5 +8,38 @@ namespace BinarySearchTreeProject
 {
     class BinarySearchTree
     {
+        int data;
+
+        node leftChild;
+        node rightChild;
+
+
+    }
+
+
+    public void InsertAtEnd(int data)
+    {
+        // root is begining of tree 
+        //key and value are pairs
+        if (root == null)
+        {
+            
+            return new node;
+
+        }
+        if (key < node ->key)
+        {
+            //if the number is less then the root node send it left
+            //insert: i guess by looking at examples on line will need instrction
+            node->leftChild = insert(node->leftChild, key);
+
+        }
+        else if (key-> node->)
+        {
+            //if the number is greater then the root sent it right 
+            node->rightChild = insert(node->rightChild, key);
+        }
+        return node;
+
     }
 }
